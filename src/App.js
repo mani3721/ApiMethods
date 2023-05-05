23420809df8da9e83ab69cc4b3d621f1e3a814d1
Manikandan P <110666644+mani3721@users.noreply.github.com>
2023-05-05 15:34:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
-import logo from './logo.svg';
+
 import './App.css';
 import CRUDAPI from './Components/AXIOS';
-import XHRMethod from './Components/XhrMethod';
-import SuperAgent from './Components/SuperAgent';
-import QwestMethod from './Components/Qwest';
-import FetchMethod from './Components/Fetch';
 
 
 
@@ -12,11 +8,9 @@ import FetchMethod from './Components/Fetch';
 function App() {
   return (
     <div className="App">
-        {/* <XHRMethod/> */}
-        {/* <FetchMethod/> */}
+
         <CRUDAPI/>
-        {/* <SuperAgent/> */}
-        {/* <QwestMethod/> */}
+
     </div>
   );
 }
